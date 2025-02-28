a1624552035567022f5cb60804b2eefb32908bb4
amirantevzadze <amextevz@yahoo.com>
2025-02-28 15:27:53 -0500

    3.1 commit

--- src/pages/store/cart_page.py
+++ src/pages/store/cart_page.py
@@ -4,6 +4,7 @@ import time
 from src.pages.base_page import BasePage
 from src.locators.store_locators import ModifierLocators,CalculationLocators
 import math
+from selenium.webdriver.common.by import By
 
 
 class CartPage(BasePage):
@@ -63,91 +64,91 @@ class CartPage(BasePage):
         self.click(ModifierLocators.PAY_NOW_BUTTON)
 
 
-    # def cart_calculations(self):
-    #     cart_items = []
-    #     main_items = self.find_elements(CalculationLocators.MAIN_ITEMS)
-    #     for item in main_items:
-    #         if "modifier" in item.get_attribute("class") and not "has-modifiers" in item.get_attribute("class"):
-    #             continue
-    #
-    #         try:
-    #             item_name = self.find_element(CalculationLocators.ITEM_NAME_1).text
-    #         except:
-    #             try:
-    #                 item_name = self.find_element(CalculationLocators.ITEM_NAME_2).text
-    #             except:
-    #                 item_name = "Unknown Item"
-    #
-    #         try:
-    #             price_element = self.find_element(CalculationLocators.ITEM_PRICE_1)
-    #             price = price_element.text
-    #         except:
-    #             try:
-    #                 price_element = self.find_element(CalculationLocators.ITEM_PRICE_2)
-    #                 price = price_element.text
-    #             except:
-    #                 price = "Price not found"
-    #
-    #         item_data = {
-    #             "name": item_name,
-    #             "price": price,
-    #             "modifiers": []
-    #         }
-    #
-    #         if "has-modifiers" in item.get_attribute("class"):
-    #             next_element = self.find_element(CalculationLocators.NEXT_ITEM)
-    #             while next_element and "modifier" in next_element.get_attribute(
-    #                     "class") and not "has-modifiers" in next_element.get_attribute("class"):
-    #                 try:
-    #                     modifier_name = self.find_element(CalculationLocators.MODIFIER_NAME).text
-    #                 except:
-    #                     modifier_name = "Unknown Modifier"
-    #
-    #                 try:
-    #                     modifier_price = self.find_element(CalculationLocators.MODIFIER_PRICE_1).text
-    #                 except:
-    #                     try:
-    #                         modifier_price = self.find_element(CalculationLocators.MODIFIER_PRICE_2).text
-    #                     except:
-    #                         modifier_price = "Price not found"
-    #
-    #                 item_data["modifiers"].append({
-    #                     "name": modifier_name,
-    #                     "price": modifier_price
-    #                 })
-    #
-    #                 try:
-    #                     next_element = self.find_element(CalculationLocators.NEXT_ITEM)
-    #                 except:
-    #                     break
-    #
-    #         cart_items.append(item_data)
-    #
-    #     script_subtotal = 0
-    #     for item in cart_items:
-    #         total_item_price = 0
-    #         print(f"\nItem: {item['name']}")
-    #         print(f"Price: {item['price']}")
-    #         item_price_float = float(item['price'].split('$')[1])
-    #         total_item_price += item_price_float
-    #
-    #         if item['modifiers']:
-    #             print("Modifiers:")
-    #             for modifier in item['modifiers']:
-    #                 mod_price = modifier['price'] if modifier['price'] else 'No additional cost'
-    #                 print(f"  - {modifier['name']}: {mod_price}")
-    #                 if '$' in mod_price:
-    #                     modifier_price_float = float(modifier['price'].split('$')[1])
-    #                     total_item_price += modifier_price_float
-    #                     print(f"Total Price for the item - ${total_item_price}")
-    #         else:
-    #             print("No modifiers")
-    #             print(f"Total Price for the item - ${total_item_price}")
-    #         print("-" * 40)
-    #         script_subtotal += total_item_price
-    #         script_subtotal = math.ceil(script_subtotal * 100) / 100
-    #     print(f"Total: ${script_subtotal}")
-    #     app_subtotal = self.find_element(*CalculationLocators.SUBTOTAL).text
-    #     print(f"SubTotal2 - {app_subtotal}")
-    #
-    #     return script_subtotal, float(app_subtotal.replace('$', ''))
+    def cart_calculations(self):
+        cart_items = []
+        main_items = self.driver.find_elements(*CalculationLocators.MAIN_ITEMS)
+        for item in main_items:
+            if "modifier" in item.get_attribute("class") and not "has-modifiers" in item.get_attribute("class"):
+                continue
+
+            try:
+                item_name = item.find_element(By.CSS_SELECTOR, "h3.cart-title").text
+            except:
+                try:
+                    item_name = item.find_element(By.CSS_SELECTOR, "h3.typography-text-p3.cart-title").text
+                except:
+                    item_name = "Unknown Item"
+
+            try:
+                price_element = item.find_element(By.CSS_SELECTOR, "div.table-col-cart-4 p")
+                price = price_element.text
+            except:
+                try:
+                    price_element = item.find_element(By.CSS_SELECTOR, "div.table-col-cart-4.item-price")
+                    price = price_element.text
+                except:
+                    price = "Price not found"
+
+            item_data = {
+                "name": item_name,
+                "price": price,
+                "modifiers": []
+            }
+
+            if "has-modifiers" in item.get_attribute("class"):
+                next_element = item.find_element(By.XPATH, "following-sibling::li[1]")
+                while next_element and "modifier" in next_element.get_attribute(
+                        "class") and not "has-modifiers" in next_element.get_attribute("class"):
+                    try:
+                        modifier_name = next_element.find_element(By.CSS_SELECTOR, "div.table-col-cart-10").text
+                    except:
+                        modifier_name = "Unknown Modifier"
+
+                    try:
+                        modifier_price = next_element.find_element(By.CSS_SELECTOR, "p.price.modifier-price").text
+                    except:
+                        try:
+                            modifier_price = next_element.find_element(By.CSS_SELECTOR, "div.table-col-cart-4").text
+                        except:
+                            modifier_price = "Price not found"
+
+                    item_data["modifiers"].append({
+                        "name": modifier_name,
+                        "price": modifier_price
+                    })
+
+                    try:
+                        next_element = next_element.find_element(By.XPATH, "following-sibling::li[1]")
+                    except:
+                        break
+
+            cart_items.append(item_data)
+
+        script_subtotal = 0
+        for item in cart_items:
+            total_item_price = 0
+            print(f"\nItem: {item['name']}")
+            print(f"Price: {item['price']}")
+            item_price_float = float(item['price'].split('$')[1])
+            total_item_price += item_price_float
+
+            if item['modifiers']:
+                print("Modifiers:")
+                for modifier in item['modifiers']:
+                    mod_price = modifier['price'] if modifier['price'] else 'No additional cost'
+                    print(f"  - {modifier['name']}: {mod_price}")
+                    if '$' in mod_price:
+                        modifier_price_float = float(modifier['price'].split('$')[1])
+                        total_item_price += modifier_price_float
+                        print(f"Total Price for the item - ${total_item_price}")
+            else:
+                print("No modifiers")
+                print(f"Total Price for the item - ${total_item_price}")
+            print("-" * 40)
+            script_subtotal += total_item_price
+            script_subtotal = math.ceil(script_subtotal * 100) / 100
+        print(f"Script calculated total: ${script_subtotal}")
+        app_subtotal = self.driver.find_element(*CalculationLocators.SUBTOTAL).text
+        print(f"App provided total: ${app_subtotal}")
+
+        return script_subtotal, float(app_subtotal.replace('$', ''))
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,13 +1,10 @@
 import time
 import pytest
-from selenium.webdriver.common.by import By
-import math
 from conftest import driver
 from src.pages.store.menu_page import MenuPage
 from src.pages.store.cart_page import CartPage
 from src.pages.store.payment_page import CheckoutPage
 from src.utils.config_reader import read_store_data
-from src.locators.store_locators import CalculationLocators
 import os
 
 
@@ -22,7 +19,6 @@ def get_all_store_ids():
 @pytest.mark.calculations
 @pytest.mark.parametrize('store_id', get_all_store_ids())
 def test_price_calculation(driver, store_id):
-    cart_items = []
     menu_page = MenuPage(driver)
     cart_page = CartPage(driver)
     payment_page = CheckoutPage(driver)
@@ -33,118 +29,8 @@ def test_price_calculation(driver, store_id):
         menu_page.select_random_item()
         cart_page.add_to_cart()
     cart_page.go_to_cart()
-    # script_calculated_subtotal, app_calculated_subtotal = cart_page.cart_calculations()
+    
+    script_calculated_subtotal, app_calculated_subtotal = cart_page.cart_calculations()
+    assert script_calculated_subtotal == app_calculated_subtotal
 
-    # assert script_calculated_subtotal == app_calculated_subtotal
-    main_items = driver.find_elements(By.CSS_SELECTOR, "li.table-row")
-    for item in main_items:
-        if "modifier" in item.get_attribute("class") and not "has-modifiers" in item.get_attribute("class"):
-            continue
-
-        try:
-            item_name = item.find_element(By.CSS_SELECTOR, "h3.cart-title").text
-        except:
-            try:
-                item_name = item.find_element(By.CSS_SELECTOR, "h3.typography-text-p3.cart-title").text
-            except:
-                item_name = "Unknown Item"
-
-        try:
-            price_element = item.find_element(By.CSS_SELECTOR, "div.table-col-cart-4 p")
-            price = price_element.text
-        except:
-            try:
-                price_element = item.find_element(By.CSS_SELECTOR, "div.table-col-cart-4.item-price")
-                price = price_element.text
-            except:
-                price = "Price not found"
-
-        item_data = {
-            "name": item_name,
-            "price": price,
-            "modifiers": []
-        }
-
-        if "has-modifiers" in item.get_attribute("class"):
-            next_element = item.find_element(By.XPATH, "following-sibling::li[1]")
-            while next_element and "modifier" in next_element.get_attribute(
-                    "class") and not "has-modifiers" in next_element.get_attribute("class"):
-                try:
-                    modifier_name = next_element.find_element(By.CSS_SELECTOR, "div.table-col-cart-10").text
-                except:
-                    modifier_name = "Unknown Modifier"
-
-                try:
-                    modifier_price = next_element.find_element(By.CSS_SELECTOR, "p.price.modifier-price").text
-                except:
-                    try:
-                        modifier_price = next_element.find_element(By.CSS_SELECTOR, "div.table-col-cart-4").text
-                    except:
-                        modifier_price = "Price not found"
-
-                item_data["modifiers"].append({
-                    "name": modifier_name,
-                    "price": modifier_price
-                })
-
-                try:
-                    next_element = next_element.find_element(By.XPATH, "following-sibling::li[1]")
-                except:
-                    break
-
-        cart_items.append(item_data)
-
-    script_subtotal = 0
-    for item in cart_items:
-        total_item_price = 0
-        print(f"\nItem: {item['name']}")
-        print(f"Price: {item['price']}")
-        item_price_float = float(item['price'].split('$')[1])
-        total_item_price += item_price_float
-
-        if item['modifiers']:
-            print("Modifiers:")
-            for modifier in item['modifiers']:
-                mod_price = modifier['price'] if modifier['price'] else 'No additional cost'
-                print(f"  - {modifier['name']}: {mod_price}")
-                if '$' in mod_price:
-                    modifier_price_float = float(modifier['price'].split('$')[1])
-                    total_item_price += modifier_price_float
-                    print(f"Total Price for the item - ${total_item_price}")
-        else:
-            print("No modifiers")
-            print(f"Total Price for the item - ${total_item_price}")
-        print("-" * 40)
-        script_subtotal += total_item_price
-        script_subtotal = math.ceil(script_subtotal * 100) / 100
-    print(f"Script calculated total: ${script_subtotal}")
-    app_subtotal = driver.find_element(*CalculationLocators.SUBTOTAL).text
-    print(f"App provided total - ${app_subtotal}")
-    assert script_subtotal == float(app_subtotal)
-
-
-
-    time.sleep(5)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    time.sleep(5) 
